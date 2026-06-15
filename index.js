5404524473f76b46e82d44e4f7a6c6211a8a8a16
alfredreturnbloxy-code <alfredreturnbloxy@gmail.com>
2026-06-15 15:18:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,32 @@ app.post("/send", async (req, res) => {
 client.login(process.env.BOT_TOKEN);
 
 app.listen(3000, () => console.log("API running"));
+
+require("dotenv").config();
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const app = express();
+app.use(express.json());
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online as ${client.user.tag}`);
+});
+
+app.post("/username-found", async (req, res) => {
+  const { username } = req.body;
+
+  const channel = await client.channels.fetch("YOUR_CHANNEL_ID");
+
+  await channel.send(`🟢 Available Roblox username found: **${username}**`);
+
+  res.sendStatus(200);
+});
+
+client.login(process.env.BOT_TOKEN);
+
+app.listen(3000, () => console.log("API running on port 3000"));
